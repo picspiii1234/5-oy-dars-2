f4c7731513d490b9db3c2dc200ee0f5a472a079b
picspiii1234 <ibrihim539@gmail.com>
2025-05-13 18:56:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,63 @@
+
+// let mars = "Hello";
+// let bu = 123;
+// let kelajak = true;
+// let son = 123n;
+// let marsItSchool;
+// let frontend = null;
+// let salomDunyo = Symbol("id");
+// let obj = { name: "Ali" };
+
+// console.log(typeof mars);
+// console.log( typeof bu);
+// console.log(typeof kelajak);
+// console.log(typeof son);
+// console.log(typeof marsItSchool);
+// console.log(typeof frontend);
+// console.log(typeof salomDunyo);
+// console.log(typeof obj);
+
+
+
+
+// let ism = prompt('Ism kiritin')
+// let belgi = prompt('harif kiritin')
+// console.log(malumot);
+
+// if (ism (belgi)) {
+//     alert('Siz kiritgan ma\'lumot string tipida')
+// } else {
+//     alert('String emas')
+// }
+
+
+// let ism = prompt('ism kiriting')
+
+// let obj = {
+//     name:ism,
+//     age:'23' ,
+//     status:'student',
+//     jop:'Frontend Developer',    
+// }
+
+// delete obj.jop;
+
+// obj.status.jop;
+// console.log(obj);
+
+
+
+let str = prompt('String tipidagi malumot kiritin')
+
+
+let nub = prompt('Number tipidagi malumot kiritin')
+
+let bln = prompt('booleon tipidagi malumot kiritin')
+
+alert('Hama malumotlarni togri kiritkaningizga ishonchingiz komilmi ')
+
+alert  ('Siz kiritgan ' +( str ) +' sozi string tipida ' +'  Siz kiritgan ' +( nub ) +' sozi number tipida ' +
+' Siz kiritgan ' +( bln ) +' sozi booleon tipida ' 
+
+)
+
